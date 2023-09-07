db2ac945b3e13a893139d2ca12d57a5fe402e112
Jakub Ďuriš <jakubduris1@gmail.com>
2023-09-07 17:46:40 +0200

    Refactored main file

--- main.py
+++ main.py
@@ -12,36 +12,32 @@ folders = [i+ '_' + j for i in maps for j in dates]
 performance = {}
 
 
-def setupPerformanceDict(performance, csv_file):
-  csv_reader = csv.reader(csv_file, delimiter=',')
-  for row in csv_reader:
-    performance[row[0]] = {"kills":  0, "assists": 0, "deaths": 0, 'matches': 0}
-
-def addToPerformanceDict(performance, csv_file):
+def setupPerformanceDict(performance, file_url):
+  with open(file_url) as csv_file:
     csv_reader = csv.reader(csv_file, delimiter=',')
     for row in csv_reader:
-      performance[row[0]] = {"kills":  performance[row[0]]["kills"] + int(row[len(row)-5]), 
-                             "assists": performance[row[0]]["assists"] +  int(row[len(row)-4]), 
-                             "deaths": performance[row[0]]["deaths"] + int(row[len(row)-3]), 
-                             'matches': performance[row[0]]['matches'] + 1}
+      performance[row[0]] = {"kills":  0, "assists": 0, "deaths": 0, 'matches': 0}
+
+def addToPerformanceDict(performance, file_url):
+    with open(file_url) as csv_file:
+      csv_reader = csv.reader(csv_file, delimiter=',')
+      for row in csv_reader:
+        performance[row[0]] = {"kills":  performance[row[0]]["kills"] + int(row[len(row)-5]), 
+                              "assists": performance[row[0]]["assists"] +  int(row[len(row)-4]), 
+                              "deaths": performance[row[0]]["deaths"] + int(row[len(row)-3]), 
+                              'matches': performance[row[0]]['matches'] + 1}
 
 for folder in os.listdir("./"):
     pngs = glob.glob("./" + folder +"/*.png")
 
     if len(pngs) == 0 and folder in folders:
         for matchFolder in os.listdir("./" + folder):
-          with open("./" + folder+'/'+  matchFolder + '/'+ team1) as csv_file:
-            setupPerformanceDict(performance, csv_file)
-
-          with open("./" + folder+'/'+  matchFolder + '/'+ team2) as csv_file:
-            setupPerformanceDict(performance, csv_file)
+            setupPerformanceDict(performance, "./" + folder+'/'+  matchFolder + '/'+ team1)
+            setupPerformanceDict(performance, "./" + folder+'/'+  matchFolder + '/'+ team2)
     else:
       if folder in folders:
-        with open("./" + folder+'/'+ team1) as csv_file:
-          setupPerformanceDict(performance, csv_file)
-     
-        with open("./" + folder+'/'+ team2) as csv_file:
-          setupPerformanceDict(performance, csv_file)
+          setupPerformanceDict(performance, "./" + folder+'/'+ team1)
+          setupPerformanceDict(performance, "./" + folder+'/'+ team2)
 
           
 for folder in os.listdir("./"):
@@ -49,19 +45,13 @@ for folder in os.listdir("./"):
 
     if len(pngs) == 0 and folder in folders:
         for matchFolder in os.listdir("./" + folder):
-          with open("./" + folder+'/'+  matchFolder + '/'+ team1) as csv_file:
-            addToPerformanceDict(performance, csv_file)
-            
-          with open("./" + folder+'/'+  matchFolder + '/'+ team2) as csv_file:
-           addToPerformanceDict(performance, csv_file)
+            addToPerformanceDict(performance,"./" + folder+'/'+  matchFolder + '/'+ team1)
+            addToPerformanceDict(performance, "./" + folder+'/'+  matchFolder + '/'+ team2)
 
     else:
       if folder in folders:
-        with open("./" + folder+'/'+ team1) as csv_file:
-          addToPerformanceDict(performance, csv_file)    
-
-        with open("./" + folder+'/'+ team2) as csv_file:
-         addToPerformanceDict(performance, csv_file)
+          addToPerformanceDict(performance, "./" + folder+'/'+ team1)    
+          addToPerformanceDict(performance, "./" + folder+'/'+ team2)
 
           
 for key in performance:
